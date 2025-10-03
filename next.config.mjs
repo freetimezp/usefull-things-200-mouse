991838f0a25396069c07efb37515dccbc0553b9c
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-10-03 18:33:21 +0300

    04. usefull-things-200-mouse. fix.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
     reactStrictMode: true,
     devIndicators: false,
+    output: "export", // Enables static exports
+    basePath: isProd ? "/usefull-things-200-mouse" : "", // Replace with your repository name
+    assetPrefix: isProd ? "/usefull-things-200-mouse/" : "", // Replace with your repository name
+    images: {
+        unoptimized: true, // Optional: Disable default image optimization if not using a CDN
+    },
 };
 
 export default nextConfig;
